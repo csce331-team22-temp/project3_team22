5dcab09f7f5fbfa36b4a042b63bf74a301015fd4
Neel <neelsatasia1221@tamu.edu>
2025-03-29 13:36:32 -0500

    Added inventory functionality

--- public/scripts/managerdashboard.js
+++ public/scripts/managerdashboard.js
@@ -1,3 +1,7 @@
 document.getElementById("staffBtn").addEventListener("click", async function () {
     window.location.href = "/staff/page";
 });
+
+document.getElementById("inventoryBtn").addEventListener("click", async function () {
+    window.location.href = "/staff/inventory/page";
+});
