e895d4ed9290242bea160c09a29377db294615c6
Isaac Ernst <6Stringmastermind@gmail.com>
2025-04-25 14:44:45 -0500

    Sets up menu board with API routes

--- routes/Home.js
+++ routes/Home.js
@@ -8,4 +8,15 @@ router.get('/', (req, res) => {
     res.render('home', { loginMessage }); 
 })
 
+router.get('/menu-board', async (req, res) => {
+    try {
+      
+        res.render('menu-board', { title: 'Menu Board' });
+
+    } catch (error) {
+        console.error('Database query failed:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 module.exports = router;
